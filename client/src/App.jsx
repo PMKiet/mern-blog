231a71b12fa6158ff96a8f398a53f6d1a09f36fe
PMKiet <minhkiet3466@gmail.com>
2024-04-26 17:11:53 +0700

    create header and selection

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Home from './pages/Home'
 import Project from './pages/Project'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,56 @@
+import { Button, Navbar, TextInput } from "flowbite-react"
+import { Link, NavLink, useLocation } from "react-router-dom"
+import { IoSearchOutline } from "react-icons/io5"
+import { FaMoon } from "react-icons/fa"
+
+export default function Header() {
+    const path = useLocation().pathname
+    return (
+        <Navbar className="border-b-2">
+            <Link to='/' className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+                <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">MERN</span>
+                Blog
+            </Link>
+            <form >
+                <TextInput
+                    type="text"
+                    placeholder="Search..."
+                    rightIcon={IoSearchOutline}
+                    className="hidden lg:inline"
+                />
+            </form>
+
+            <Button className="w-12 h-10 outline-none lg:hidden" color='gray' pill>
+                <IoSearchOutline />
+            </Button>
+            <div className="flex gap-2 md:order-2">
+                <Button className="w-12 h-10 sm:inline hidden" color='gray' pill>
+                    <FaMoon />
+                </Button>
+                <Link to='/sign-in'>
+                    <Button gradientDuoTone='purpleToBlue'>
+                        Sign In
+                    </Button>
+                </Link>
+                <Navbar.Toggle />
+            </div>
+            <Navbar.Collapse>
+                <Navbar.Link as='div' active={path === '/'}>
+                    <Link to='/'>
+                        Home
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link as='div' active={path === '/about'}>
+                    <Link to='/about'>
+                        About
+                    </Link>
+                </Navbar.Link>
+                <Navbar.Link as='div' active={path === '/project'}>
+                    <Link to='/project'>
+                        Project
+                    </Link>
+                </Navbar.Link>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+}
