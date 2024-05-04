2544a68b27ad1c98b081794767742068c340a217
PMKiet <minhkiet3466@gmail.com>
2024-05-04 13:59:43 +0700

    add update router api router

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,43 @@
+import User from "../models/user.model.js"
+import { errorHandler } from "../utils/error.js"
+import bcryptjs from 'bcryptjs'
+
 export const test = (req, res) => {
     res.json({ message: 'hello word' })
+}
+
+export const updateUser = async (req, res, next) => {
+    console.log(req.body);
+    if (req.user.id !== req.params.userId) {
+        return next(errorHandler(403), 'You are not allowed to update user')
+    }
+
+    if (req.body.password) {
+        if (req.body.password.length < 6) {
+            return next(errorHandler(400, 'Password must be at least 6 characters'))
+        }
+        req.body.password = bcryptjs.compare(password, 10)
+    }
+
+    if (req.body.username.length < 7 || req.body.username.length > 20) {
+        return next(errorHandler(400, 'Username must be between7 and 20 characters'))
+    }
+
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+        return next(errorHandler(400, 'Username must be contain letters and numbers'))
+    }
+    try {
+        const updateUser = await User.findByIdAndUpdate(req.params.userId, {
+            $set: {
+                username: req.body.username,
+                password: req.body.password,
+                profilePicture: req.body.profilePicture,
+                email: req.body.email
+            },
+        }, { new: true })
+        const { password, ...rest } = updateUser._doc
+        res.status(200).json(rest)
+    } catch (error) {
+        console.log(error);
+    }
 }
