6840f9575a822efd540c636e622fefecc232a541
PMKiet <minhkiet3466@gmail.com>
2024-05-07 16:01:53 +0700

    add show more func to post result of the dashboard

--- client/src/components/DashPost.jsx
+++ client/src/components/DashPost.jsx
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom"
 export default function DashPost() {
     const { currentUser } = useSelector((state) => state.User)
     const [userPosts, setUserPosts] = useState([])
+    const [showMore, setShowMore] = useState(true)
     console.log(userPosts)
 
     useEffect(() => {
@@ -15,6 +16,9 @@ export default function DashPost() {
                 const data = await res.json()
                 if (res.ok) {
                     setUserPosts(data.posts)
+                    if (data.posts.length < 9) {
+                        setShowMore(false)
+                    }
                 }
             } catch (error) {
                 console.log(error)
@@ -24,50 +28,76 @@ export default function DashPost() {
             fetchPosts()
         }
     }, [currentUser._id])
+    const handleShowMore = async () => {
+        const startIndex = userPosts.length
+        try {
+            const res = await fetch(`/api/post/getposts?userId=${currentUser._id}&startIndex=${startIndex}`)
+            const data = await res.json()
+            console.log(data);
+            if (res.ok) {
+                setUserPosts((prev) => [...prev, ...data.posts])
+                if (data.posts.length < 9) {
+                    setShowMore(false)
+                }
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div className="table-auto overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300
+        <div className="table-auto overflow-x-auto md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300
         dark:scrollbar-track-slate-700 dark:scrollbar-thumb-slate-500">
             {currentUser.isAdmin && userPosts.length > 0 ? (
-                <Table hoverable className="shadow-md">
-                    <Table.Head>
-                        <Table.HeadCell>Date update</Table.HeadCell>
-                        <Table.HeadCell>Post image</Table.HeadCell>
-                        <Table.HeadCell>Post title</Table.HeadCell>
-                        <Table.HeadCell>Category</Table.HeadCell>
-                        <Table.HeadCell>Delete</Table.HeadCell>
-                        <Table.HeadCell>
-                            <span>Edit</span>
-                        </Table.HeadCell>
-                    </Table.Head>
-                    {userPosts.map((post) => (
-                        <Table.Body className="divide-y">
-                            <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
-                                <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
-                                <Table.Cell>
-                                    <Link to={`/post/${post.slug}`}>
-                                        <img
-                                            src={post.image}
-                                            alt={post.title}
-                                            className="w-20 h-10 object-cover bg-gray-500"
-                                        />
-                                    </Link>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link className="font-medium text-gray-900 dark:text-white" to={`/post/${post.slug}`}>{post.title}</Link>
-                                </Table.Cell>
-                                <Table.Cell>{post.category}</Table.Cell>
-                                <Table.Cell>
-                                    <span className="font-medium text-red-500 hover:underline cursor-pointer">Delete</span>
-                                </Table.Cell>
-                                <Table.Cell>
-                                    <Link to={`/post/${post._id}`} className="hover:underline text-teal-500">
-                                        <span>Edit</span>
-                                    </Link>
-                                </Table.Cell>
-                            </Table.Row>
-                        </Table.Body>
-                    ))}
-                </Table>
+                <>
+                    <Table hoverable className="shadow-md">
+                        <Table.Head>
+                            <Table.HeadCell>Date update</Table.HeadCell>
+                            <Table.HeadCell>Post image</Table.HeadCell>
+                            <Table.HeadCell>Post title</Table.HeadCell>
+                            <Table.HeadCell>Category</Table.HeadCell>
+                            <Table.HeadCell>Delete</Table.HeadCell>
+                            <Table.HeadCell>
+                                <span>Edit</span>
+                            </Table.HeadCell>
+                        </Table.Head>
+                        {userPosts.map((post, i) => (
+                            <Table.Body key={i} className="divide-y">
+                                <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800">
+                                    <Table.Cell>{new Date(post.updatedAt).toLocaleDateString()}</Table.Cell>
+                                    <Table.Cell>
+                                        <Link to={`/post/${post.slug}`}>
+                                            <img
+                                                src={post.image}
+                                                alt={post.title}
+                                                className="w-20 h-10 object-cover bg-gray-500"
+                                            />
+                                        </Link>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link className="font-medium text-gray-900 dark:text-white" to={`/post/${post.slug}`}>{post.title}</Link>
+                                    </Table.Cell>
+                                    <Table.Cell>{post.category}</Table.Cell>
+                                    <Table.Cell>
+                                        <span className="font-medium text-red-500 hover:underline cursor-pointer">Delete</span>
+                                    </Table.Cell>
+                                    <Table.Cell>
+                                        <Link to={`/post/${post._id}`} className="hover:underline text-teal-500">
+                                            <span>Edit</span>
+                                        </Link>
+                                    </Table.Cell>
+                                </Table.Row>
+                            </Table.Body>
+                        ))}
+                    </Table>
+                    {
+                        showMore && (
+                            <button onClick={handleShowMore} className="w-full text-teal-500 self-center text-sm py-7">
+                                Show more
+                            </button>
+                        )
+                    }
+                </>
             ) : (
                 <p>You have no post</p>
             )
