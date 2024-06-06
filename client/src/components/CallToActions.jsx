f2a04533e80b7f21865406f7a6174453dd52b951
PMKiet <minhkiet3466@gmail.com>
2024-06-06 15:37:32 +0700

    complete home page

--- client/src/components/CallToActions.jsx
+++ client/src/components/CallToActions.jsx
@@ -1,7 +1,7 @@
 import { Button } from 'flowbite-react'
 import React from 'react'
 
-export default function aCallToActions() {
+export default function CallToActions() {
     return (
         <div
             className="flex flex-col  sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl
