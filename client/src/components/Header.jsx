85a9002bbaeb8cba794aff879dcf0ae344e2f28f
PMKiet <minhkiet3466@gmail.com>
2024-06-09 16:03:19 +0700

    complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,32 @@
 import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react"
-import { Link, useLocation } from "react-router-dom"
+import { Link, useLocation, useNavigate } from "react-router-dom"
 import { IoSearchOutline } from "react-icons/io5"
 import { FaMoon } from "react-icons/fa"
 import { useSelector, useDispatch } from 'react-redux'
 import { toggleTheme } from "../redux/Theme/themeSlice"
 import { signOutSuccess } from "../redux/user/userSlice"
+import { useEffect, useState } from "react"
 
 export default function Header() {
+    const location = useLocation()
+    const navigate = useNavigate()
     const dispatch = useDispatch()
     const currentUser = useSelector((state) => state.User.currentUser)
     const path = useLocation().pathname
+    const [searchTerm, setSearchTerm] = useState('')
+
+    console.log(searchTerm)
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search)
+        const searchTermFromUrl = urlParams.get('searchTerm')
+        if (searchTermFromUrl) {
+            setSearchTerm(searchTermFromUrl)
+        }
+        console.log('location.search', location.search)
+        console.log('urlParams', urlParams)
+        console.log('searchTermFromUrl', searchTermFromUrl)
+    }, [location.search])
 
     const handleSignOut = async () => {
         try {
@@ -26,19 +43,28 @@ export default function Header() {
             console.log(error)
         }
     }
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const urlParams = new URLSearchParams(location.search)
+        urlParams.set('searchTerm', searchTerm)
+        const searchQuery = urlParams.toString()
+        navigate(`/search?${searchQuery}`)
 
+    }
     return (
         <Navbar className="border-b-2">
             <Link to='/' className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
                 <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">MERN</span>
                 Blog
             </Link>
-            <form >
+            <form onSubmit={handleSubmit}>
                 <TextInput
                     type="text"
                     placeholder="Search..."
                     rightIcon={IoSearchOutline}
                     className="hidden lg:inline"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
 
