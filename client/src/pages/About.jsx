c3b04e65ba08060c04b13b1ac35737f6ba10f4b1
PMKiet <minhkiet3466@gmail.com>
2024-06-10 14:05:03 +0700

    complete about and project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 
 export default function About() {
     return (
-        <div>
-            About
+        <div className='min-h-screen flex items-center'>
+            <div className="max-w-2xl mx-auto p-3">
+                <div>
+                    <h1 className='text-3xl font font-semibold text-center my-7'>
+                        About Mern Blog
+                        <div className="text-md text-gray-500 flex flex-col gap-6">
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Hic provident non praesentium aliquam eius perspiciatis necessitatibus quidem illo itaque temporibus.
+                                Nobis quia sint maiores,
+                                delectus et aut rem quis quo.
+                            </p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Hic provident non praesentium aliquam eius perspiciatis necessitatibus quidem illo itaque temporibus.
+                                Nobis quia sint maiores,
+                                delectus et aut rem quis quo.
+                            </p>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                                Hic provident non praesentium aliquam eius perspiciatis necessitatibus quidem illo itaque temporibus.
+                                Nobis quia sint maiores,
+                                delectus et aut rem quis quo.
+                            </p>
+                        </div>
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
--- client/src/pages/Project.jsx
+++ client/src/pages/Project.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CallToActions from '../components/CallToActions.jsx'
 
 export default function Project() {
     return (
-        <div>
-            Project
+        <div className='min-h-screen max-w-2xl mx-auto flex justify-center item flex-col gap-6 p-3'>
+            <h1 className='text-3xl font-semibold'>Projects</h1>
+            <p className='text-md text-gray-500'>Build fun and engaging project while learning HTML, CSS and JAVASCRIPT</p>
+            <CallToActions />
         </div>
     )
 }
